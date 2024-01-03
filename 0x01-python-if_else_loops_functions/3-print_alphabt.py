d69e20f452d6336e607267dabb5953589f9b62e2
kyaloeriq <kyaloeriq@gmail.com>
2024-01-03 10:26:08 -0800

    prints the ASCII alphabet except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 alpbet = ''.join([chr(m) for m in range(ord('a'), ord('z') + 1)])
-filteredalpbet = ''.join(
-        [letter for letter in alpbet if letter not in ('q', 'e')]
-)
+filteredalpbet = ''
+for letter in alpbet:
+    if letter not in ('q', 'e'):
+        filteredalpbet += letter
 print("{}".format(filteredalpbet), end='')
