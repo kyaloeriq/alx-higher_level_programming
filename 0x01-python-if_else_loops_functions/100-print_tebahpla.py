556e28e190dc0e18dcdf5a63f9fcbd2806b60f79
kyaloeriq <kyaloeriq@gmail.com>
2024-01-03 14:50:18 -0800

    ASCII alphabet, in reverse order, alternating lowercase and uppercase

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-betalp = ''.join([chr(m) + chr(m - 32) for m in range(ord('z'), ord('a') - 1, -1)])
+betalp = ''.join([
+    chr(m) if m % 2 == 0 else chr(m - 32)
+    for m in range(ord('z'), ord('a') - 1, -1)
+])
 print("{}".format(betalp), end='')
