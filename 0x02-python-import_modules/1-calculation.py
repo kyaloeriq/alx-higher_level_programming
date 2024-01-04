84d1b59ce7212dcfd2b5e9637ef61d57dc513246
kyaloeriq <kyaloeriq@gmail.com>
2024-01-04 13:31:00 -0800

    imports functions from the file calculator_1.py, does some Maths

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import calculator_1 as my_module
+
+
+def main():
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, my_module.add(a, b)))
+    print("{} - {} = {}".format(a, b, my_module.sub(a, b)))
+    print("{} * {} = {}".format(a, b, my_module.mul(a, b)))
+    print("{} / {} = {}".format(a, b, my_module.div(a, b)))
+
+
+if __name__ == "__main__":
+    main()
