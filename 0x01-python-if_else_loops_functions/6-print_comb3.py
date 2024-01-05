f71d676c2833477c83677a56568e39b3c6cfc2bf
kyaloeriq <kyaloeriq@gmail.com>
2024-01-05 08:38:50 -0800

    prints all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# prints all possible combinations of two digits
+for m in range(10):
+    for n in range(10):
+        if m != n:
+            if m < 9:
+                print("{:01d}{:01d}, ".format(m, n), end='')
+            else:
+                print("{:01d}{:01d}".format(m, n))
