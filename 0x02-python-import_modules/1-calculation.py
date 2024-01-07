3cb344a4622b36b11615b5c53d9fb106637ef0af
kyaloeriq <kyaloeriq@gmail.com>
2024-01-07 04:03:14 -0800

    imports functions from the file calculator_1.py, does some Maths

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,11 +6,24 @@ def main():
 
     a = 10
     b = 5
-    # uses the two variables only, as arguments when calling functions
-    print("{} + {} = {}".format(a, b, my_module.add(a, b)))
-    print("{} - {} = {}".format(a, b, my_module.sub(a, b)))
-    print("{} * {} = {}".format(a, b, my_module.mul(a, b)))
-    print("{} / {} = {}".format(a, b, my_module.div(a, b)))
+
+    # Import specific functions from my_module
+    add_function = my_module.add
+    sub_function = my_module.sub
+    mul_function = my_module.mul
+    div_function = my_module.div
+
+    # Call functions with variables as arguments
+    sum_rslt = add_function(a, b)
+    diff_rslt = sub_function(a, b)
+    mul_rslt = mul_function(a, b)
+    div_rslt = div_function(a, b)
+
+    # Print the results
+    print("{} + {} = {}".format(a, b, sum_rslt))
+    print("{} - {} = {}".format(a, b, diff_rslt))
+    print("{} * {} = {}".format(a, b, mul_rslt))
+    print("{} / {} = {}".format(a, b, div_rslt))
 
 
 if __name__ == "__main__":
