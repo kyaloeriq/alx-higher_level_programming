3f9c9d584e952356d0cd90d2d8a1f29b73ffa582
kyaloeriq <kyaloeriq@gmail.com>
2024-01-08 00:18:41 -0800

    prints all integers of a list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=None):
-    my_list = my_list or []
+def print_list_integer(my_list=[]):
     for num in my_list:
         print("{}".format(num))
 
