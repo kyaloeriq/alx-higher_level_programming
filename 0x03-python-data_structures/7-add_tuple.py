fdc0f1dccf318484a4bd3dc93bd29b2e96566480
kyaloeriq <kyaloeriq@gmail.com>
2024-01-08 23:26:33 -0800

    adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
     tuple_b = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
-    
     sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return sum_tuple
 
