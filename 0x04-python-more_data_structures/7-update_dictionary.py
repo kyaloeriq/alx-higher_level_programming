f6fb0bed85e6f48f75b09e7665296b92f30b68ce
kyaloeriq <kyaloeriq@gmail.com>
2024-01-10 13:32:04 -0800

    replaces or adds key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary):
+        print("{}: {}".format(key, a_dictionary[key]))
