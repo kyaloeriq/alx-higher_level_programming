abc5175fcb2c5463643f88057bc1ae31d38173e3
kyaloeriq <kyaloeriq@gmail.com>
2024-01-10 06:56:04 -0800

    returns a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    diff_set = set()
+    for i in set_1:
+        if i not in set_2:
+            diff_set.add(i)
+    for j in set_2:
+        if j not in set_1:
+            diff_set.add(j)
+    return diff_set
+
+
+if __name__ == "__main__":
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
+    od_set = only_diff_elements(set_1, set_2)
+    print(sorted(list(od_set)))
