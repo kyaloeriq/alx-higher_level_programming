fa1fa0210a08014657054044ae1a75886866b4c1
kyaloeriq <kyaloeriq@gmail.com>
2024-01-12 02:17:51 -0800

    returns a new dictionary with all values multiplied by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
+
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary):
+        print("{}: {}".format(key, a_dictionary[key]))
+
+
+if __name__ == "__main__":
+    a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+    new_dict = multiply_by_2(a_dictionary)
+    print_sorted_dictionary(a_dictionary)
+    print("--")
+    print_sorted_dictionary(new_dict)
