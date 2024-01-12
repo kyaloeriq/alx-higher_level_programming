a90d9867d120178b7c78f7fe74dbf69c6aec8fe1
kyaloeriq <kyaloeriq@gmail.com>
2024-01-12 08:16:20 -0800

    converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman_numerals = {
-            'I': 1, 
-            'V': 5,  
+            'I': 1,
+            'V': 5,
             'X': 10,
             'L': 50,
             'C': 100,
