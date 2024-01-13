51079a1aae0a378596b97354ac74e6a933e6c137
kyaloeriq <kyaloeriq@gmail.com>
2024-01-13 05:19:09 -0800

    prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # generate the alphabet and filter out e and q
 def print_alphabt():
-    for char in range(ord('a'), ord('z') + 1):
-        if chr(char) != 'e' and chr(char) != 'q':
-            print(chr(char), end='')
+    print(
+            ''.join(chr(char) for char in range(ord('a'), ord('z') + 1)
+            if chr(char) != 'e' and chr(char) != 'q')
+            )
 
 
 print_alphabt()
