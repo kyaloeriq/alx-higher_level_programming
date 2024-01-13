9c0c774a7101e7c31ca6d7b4b2ae9454505bf271
kyaloeriq <kyaloeriq@gmail.com>
2024-01-13 04:51:29 -0800

    checks for lowercase character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,3 +4,11 @@ def islower(c):
         if c == chr(char):
             return True
     return False
+
+
+if __name__ == "__main__":
+    print("a is {}".format("lower" if islower("a") else "upper"))
+    print("H is {}".format("lower" if islower("H") else "upper"))
+    print("A is {}".format("lower" if islower("A") else "upper"))
+    print("3 is {}".format("lower" if islower("3") else "upper"))
+    print("g is {}".format("lower" if islower("g") else "upper"))
