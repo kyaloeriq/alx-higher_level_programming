b5480b26b418b57d062e84e67dc021d6f41bd265
kyaloeriq <kyaloeriq@gmail.com>
2024-01-14 01:43:44 -0800

    prints a string in uppercase followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# prints string in uppercase followed by a new line
+def uppercase(str_input):
+    rslt_str = ''
+    for char in str_input:
+        if 'a' <= char <= 'z':
+            # Convert lowercase to uppercase
+            rslt_str += chr(ord(char) - ord('a') + ord('A'))
+        else:
+            # Keep non-alphabetic characters unchanged
+            rslt_str += char
+    print(rslt_str)
+
+
+if __name__ == "__main__":
+    uppercase("best")
+    uppercase("Best School 98 Battery street")
