629eb0aa04dbda5b27232534ac3c5caac9c08231
kyaloeriq <kyaloeriq@gmail.com>
2024-01-15 05:04:44 -0800

    Fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for a in range(1, 100):
+        if a % 3 == 0 and a % 5 != 0:
+            print("Fizz ", end= '')
+        elif a % 5 == 0 and a % 3 != 0:
+            print("Buzz ", end= '')
+        elif a % 3 == 0 and a % 5 == 0:
+            print("Fizzbuzz ", end= '')
+        else:
+            print(a , end= '')
+
+
+if __name__ == "__main__":
+    fizzbuzz()
+    print("")
