3268a7e3eb8819c01e6f9681184919f7f8703b51
kyaloeriq <kyaloeriq@gmail.com>
2024-01-18 06:11:46 -0800

    object-oriented programming with Python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
     language that combines remarkable power with very clear syntax"
-print(open(__file__).readlines()[1].split('"')[1][39:66], "with Python")
+print(open(__file__).read().split('\"')[1][39:66], "with Python")
 # print(str)
