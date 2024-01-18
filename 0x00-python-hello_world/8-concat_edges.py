aaa82d84446776e714f547571ce11fe1f1d5917c
kyaloeriq <kyaloeriq@gmail.com>
2024-01-18 05:26:03 -0800

    object-oriented programming with Python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
     language that combines remarkable power with very clear syntax"
-print(f"{str[41:63]}with Python")
+print(f"{str[str.find('object-oriented proramming'):].split()[0]}with Python")
 # print(str)
