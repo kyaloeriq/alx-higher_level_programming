1a3881f30e3816a438f94a653ed1c9fb1eab8645
kyaloeriq <kyaloeriq@gmail.com>
2024-01-24 04:43:13 -0800

    returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""class Square that returns the current square area"""
+
+
+class Square:
+    """Instantiation with optional size"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size**2
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print("Area: {}".format(my_square_1.area()))
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    my_square_2 = Square(5)
+    print("Area: {}".format(my_square_2.area()))
