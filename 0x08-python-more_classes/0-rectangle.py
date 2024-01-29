b35fd05beb438ff4d3b6b11e41464a85d550150e
kyaloeriq <kyaloeriq@gmail.com>
2024-01-29 01:54:47 -0800

    class Rectangle that defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A class Rectangle that defines a rectangle:
+"""
+
+
+class Rectangle:
+    """
+    class Rectangle that defines a rectangle:
+    Attributes:
+        No explicit attributes defined
+    """
+    pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
