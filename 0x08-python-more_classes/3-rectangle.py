f5f20c252bc172b607057e78869bbd421ba1d2a5
kyaloeriq <kyaloeriq@gmail.com>
2024-01-29 12:14:28 -0800

    string representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""String representation module"""
+
+
+class Rectangle:
+    """class Rectangle that defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Instantiation with optional width and height"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Retrieve the value of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the value of width with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the value of height with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width + self.__height) * 2
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__width == 0 or self.__height == 0:
+            print()
+        else:
+            for _ in range(self.__height):
+                print("#" * self.__width)
+
+    def __str__(self):
+        """String representation of the rectangle"""
+        return "Rectangle(width={}, height={})".format(self.__width, self.__height)
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+    print(str(my_rectangle))
+    print(repr(my_rectangle))
+
+    print("--")
+
+    my_rectangle.width = 10
+    my_rectangle.height = 3
+    print(my_rectangle)
+    print(repr(my_rectangle))
