6ce1609c3f9fdcfd7e0fd229cfb7c60f743b845b
kyaloeriq <kyaloeriq@gmail.com>
2024-02-02 07:51:46 -0800

    function that multiplies 2 matrices

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -8,30 +8,37 @@ def matrix_mul(m_a, m_b):
     m_a and m_b must be an list of lists of integers or floats
     """
     if not isinstance(m_a, list):
-         raise TypeError("m_a must be a list")
+        raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
-         raise TypeError("m_b must be a list")
-    if m_a = []:
+        raise TypeError("m_b must be a list")
+    if m_a == []:
         raise ValueError("m_a can't be empty")
-    if m_b = []:
+    if m_b == []:
         raise ValueError("m_b can't be empty")
-    if m_a = [:] is not int or float:
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if not all(isinstance(num, (int, float)) for row in m_a for num in row):
         raise TypeError("m_a should contain only integers or floats")
-    if m_b = [:] is not int or float:
+    if not all(isinstance(num, (int, float)) for row in m_b for num in row):
         raise TypeError("m_b should contain only integers or floats")
-    if not isinstance(sizeofrows(m_a) are equal:
-         raise ValueError("each row of m_b must be of the same size")
-    if not isinstance(sizeofrows(m_a) are equal:
-         raise ValueError("each row of m_b must be of the same size"
-    if (m_a * m_b == NULL):
+    if len(set(len(row) for row in m_a)) != 1:
+        raise ValueError("each row of m_b must be of the same size")
+    if len(set(len(row) for row in m_b)) != 1:
+        raise ValueError("each row of m_b must be of the same size")
+    if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    return (m_a * m_b)
+    result = [[0 for _ in range(len(m_b[0]))] for _ in range(len(m_a))]
+    for a in range(len(m_a)):
+        for b in range(len(m_b[0])):
+            for c in range(len(m_b)):
+                result[a][b] += m_a[a][c] * m_b[c][b]
+
+    return result
 
 
 if __name__ == "__main__":
     [[7, 10], [15, 22]]
     [[13, 16]]
-
-
-
