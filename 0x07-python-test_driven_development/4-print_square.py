7b67cd7a18ff148c927d5add424610354e498007
kyaloeriq <kyaloeriq@gmail.com>
2024-02-02 00:58:36 -0800

    prints a square with the character #

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,8 +14,8 @@ def print_square(size):
     if (size < 0):
         raise ValueError("size must be >= 0")
 
-    for _ in range(size):
-        print("#" * size)
+    for i in range(size):
+            print("#" * size)
 
 
 if __name__ == "__main__":
