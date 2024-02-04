bbafc0d9f0adfb8d548a93063cc4f35566ff13ce
kyaloeriq <kyaloeriq@gmail.com>
2024-02-04 00:33:43 -0800

    function that prints My name is <first name> <last name>

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""
-prints My name is <first name> <last name>
-first_name and last_name must be strings otherwise, a TypeError exception
-"""
+"""prints My name is <first name> <last name>"""
 
 
 def say_my_name(first_name, last_name=""):
     """
     function that prints My name is <first name> <last name>
-    first_name and last_name must be strings otherwise, a TypeError exception is raised
+    first_name, last_name are strings, otherwsie TypeError exception is raised
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print (f"My name is {first_name} {last_name}")
+    print(f"My name is {first_name} {last_name}")
 
 
 if __name__ == "__main__":
