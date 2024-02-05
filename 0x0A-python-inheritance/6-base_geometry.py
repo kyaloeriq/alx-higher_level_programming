945f5189da8b07ee4a77f638ec1b662be6136894
kyaloeriq <kyaloeriq@gmail.com>
2024-02-05 11:32:39 -0800

    class BaseGeometry with public instance method

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
     class BaseGeometry
     """
     def area(self):
-        raise NotImplementedError("area() is not implemented")
+        raise Exception("area() is not implemented")
 
 
 if __name__ == "__main__":
