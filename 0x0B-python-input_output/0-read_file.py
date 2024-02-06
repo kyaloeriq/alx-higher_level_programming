5ef024e56015b3b45a0bda43aac9c24841732b70
kyaloeriq <kyaloeriq@gmail.com>
2024-02-05 22:37:25 -0800

    function that reads a text file (UTF8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Read file module"""
+
+
+def read_file(filename=""):
+    """
+    function that reads a text file (UTF8) and prints it to stdout
+    the with statement must be used
+    no need to manage file permission or file doesn't exist exceptions
+    """
+    with open(filename="", encoding="utf-8") as j:
+        read_data = j.read()
+
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
