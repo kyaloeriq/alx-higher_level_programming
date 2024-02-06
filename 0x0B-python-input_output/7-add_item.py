0b800a835782e7ca23bcdb72b6b4715ba06d78d3
kyaloeriq <kyaloeriq@gmail.com>
2024-02-06 13:53:30 -0800

    script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Load, add and save module"""
-from save_to_json_file import save_to_json_file 
+import os
+from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
 
@@ -9,6 +10,6 @@ def add_item(filename, *args):
     script that adds all arguments to a Python list,
     then save them to a file
     """
-    my_list = load_from_json_file
+    my_list = load_from_json_file(filename) if os.path.exists(filename) else []
     my_list.extend(args)
     save_to_json_file(my_list, filename)
