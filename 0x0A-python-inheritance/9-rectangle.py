66f189ed6bd72a0114b3fd9bd90470f8e53b79f4
kyaloeriq <kyaloeriq@gmail.com>
2024-02-08 00:19:05 -0800

    class Rectangle that inherits from BaseGeometry

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def __str__(self):
-        return f"[Rectangle] <{self.__width}/{self.__height}>"
+        return f"[Rectangle] {self.__width}/{self.__height}"
 
     def area(self):
         return self.__width * self.__height
