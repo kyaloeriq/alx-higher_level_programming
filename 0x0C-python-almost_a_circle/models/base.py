286e843994a3267abf1cdb5268e4d8167dd32c1a
kyaloeriq <kyaloeriq@gmail.com>
2024-02-08 03:01:12 -0800

    The Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""class Base Module"""
+
+
+class Base:
+    """The Base class"""
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+         """Instantiation with id"""
+         if id is not None:
+             self.__id = id
+         else:
+               Base.__nb_objects += 1
+               self.__id = Base.__nb_objects
+
+    @property
+    def id(self):
+        return self.__id
+
+
+if __name__ == "__main__":
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
+
+    b4 = Base(12)
+    print(b4.id)
+
+    b5 = Base()
+    print(b5.id)
