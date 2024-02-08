33d509232aeeacfcba7dc6345b2a5f17b381b6a2
kyaloeriq <kyaloeriq@gmail.com>
2024-02-08 00:12:38 -0800

    class Square that inherits from Rectangle-2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -47,7 +47,7 @@ class Square(Rectangle):
         self.__size = size
 
     def __str__(self):
-        return f"[Square] <{self.__size}/{self.__size}>"
+        return f"[Square] {self.__size}/{self.__size}"
 
     def area(self):
         return self.__size * self.__size
