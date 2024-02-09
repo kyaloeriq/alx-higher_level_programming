7ceeb2e92d22484e9a4089b033fc9b1c22a67395
kyaloeriq <kyaloeriq@gmail.com>
2024-02-09 13:28:33 -0800

    The Zen of Python

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-print("""The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.\nFlat is better than nested.\nSparse is better than dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules.\nAlthough practicality beats purity.\nErrors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.\nThere should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you're Dutch.\nNow is better than never.\nAlthough never is often better than *right* now.\nIf the implementation is hard to explain, it's a bad idea.\nIf the implementation is easy to explain, it may be a good idea.\nNamespaces are one honking great idea -- let's do more of those!""")
+zen_part1 = """The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly. """
+zen_part2 = """Explicit is better than implicit. Simple is better than complex. """
+zen_part3 = """Complex is better than complicated. Flat is better than nested. """
+zen_part4 = """Sparse is better than dense. Readability counts. Special cases aren't """
+zen_part5 = """special enough to break the rules. Although practicality beats purity. """
+zen_part6 = """Errors should never pass silently. Unless explicitly silenced. """
+zen_part7 = """In the face of ambiguity, refuse the temptation to guess. There should """
+zen_part8 = """be one-- and preferably only one --obvious way to do it. Although that """
+zen_part9 = """way may not be obvious at first unless you're Dutch. Now is better """
+zen_part10 = """than never. Although never is often better than *right* now. If the """
+zen_part11 = """implementation is hard to explain, it's a bad idea. If the """
+zen_part12 = """implementation is easy to explain, it may be a good idea. Namespaces """
+zen_part13 = """are one honking great idea -- let's do more of those!"""
+
+print(zen_part1 + zen_part2 + zen_part3 + zen_part4 + zen_part5 + zen_part6 +
+      zen_part7 + zen_part8 + zen_part9 + zen_part10 + zen_part11 + zen_part12 +
+      zen_part13)
