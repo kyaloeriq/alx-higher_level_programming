1344a463958e8685bc362c30f329205fb8988ba1
kyaloeriq <kyaloeriq@gmail.com>
2024-02-09 13:36:09 -0800

    The Zen of Python

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-zen_part1 = """The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly. """
-zen_part2 = """Explicit is better than implicit. Simple is better than complex. """
-zen_part3 = """Complex is better than complicated. Flat is better than nested. """
-zen_part4 = """Sparse is better than dense. Readability counts. Special cases aren't """
-zen_part5 = """special enough to break the rules. Although practicality beats purity. """
-zen_part6 = """Errors should never pass silently. Unless explicitly silenced. """
-zen_part7 = """In the face of ambiguity, refuse the temptation to guess. There should """
-zen_part8 = """be one-- and preferably only one --obvious way to do it. Although that """
-zen_part9 = """way may not be obvious at first unless you're Dutch. Now is better """
-zen_part10 = """than never. Although never is often better than *right* now. If the """
-zen_part11 = """implementation is hard to explain, it's a bad idea. If the """
-zen_part12 = """implementation is easy to explain, it may be a good idea. Namespaces """
-zen_part13 = """are one honking great idea -- let's do more of those!"""
+zen_of_python = '''The Zen of Python, by Tim Peters
 
-print(zen_part1 + zen_part2 + zen_part3 + zen_part4 + zen_part5 + zen_part6 +
-      zen_part7 + zen_part8 + zen_part9 + zen_part10 + zen_part11 + zen_part12 +
-      zen_part13)
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!'''
+print(zen_of_python)
