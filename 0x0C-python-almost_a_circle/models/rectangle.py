a145725e7781d1cd5bff369f38c281726437593d
kyaloeriq <kyaloeriq@gmail.com>
2024-02-09 12:19:14 -0800

    def update(self, *args): that assigns an argument to each attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,17 +75,9 @@ class Rectangle(Base):
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
 
     def update(self, *args):
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.width = args[1]
-            if len(args) >= 3:
-                self.height = args[2]
-            if len(args) >= 4:
-                self.x = args[3]
-            if len(args) >= 5:
-                self.y = args[4]
+        """Update attributes based on arguments"""
+        if len(args) >= 1:
+            self.__init__(*args)
 
         
 if __name__ == "__main__":
