a2e091715a24830975ec72c50fed5dd36c2b4a8b
kyaloeriq <kyaloeriq@gmail.com>
2024-02-12 12:15:56 -0800

    first class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,13 +7,13 @@ class Base:
     __nb_objects = 0
 
 
-def __init__(self, id=None):
-    """Instantiation with id"""
-    if id is not None:
-        self.__id = id
-    else:
-        Base.__nb_objects += 1
-        self.__id = Base.__nb_objects
+    def __init__(self, id=None):
+        """Instantiation with id"""
+        if id is not None:
+            self.__id = id
+        else:
+            Base.__nb_objects += 1
+            self.__id = Base.__nb_objects
 
     @property
     def id(self):
