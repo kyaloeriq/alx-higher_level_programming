c8eb5c8f8d4a401fa3d44aa6cf06f92cbf701ee2
kyaloeriq <kyaloeriq@gmail.com>
2024-03-24 08:00:43 -0700

    script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in the MySQL server

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- Script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in the MySQL server
+-- Script that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in MySQL server
 
 USE hbtn_0c_0;
 
