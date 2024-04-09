daafa06f3bd7ddfc0ae06240df30c74827b57c7f
kyaloeriq <kyaloeriq@gmail.com>
2024-04-09 09:58:34 -0700

    prints a message depending of the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const argv = process.argv.length;
+
+// Check the number of command-line arguments
+if (argv === 2)
+{
+	console.log('No argument');
+}
+else if (argv === 3)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Arguments found');
+}
