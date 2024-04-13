41f772c3628b3ed4368a29beed2d7892ffc4ca7b
kyaloeriq <kyaloeriq@gmail.com>
2024-04-13 00:59:26 -0700

    empty class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -5,9 +5,3 @@ class Rectangle {
   // Constructor is empty for now
   }
 }
-
-// Create an instance of Rectangle
-const r1 = new Rectangle();
-
-// Print the instance to verify it's created
-console.log(r1);
