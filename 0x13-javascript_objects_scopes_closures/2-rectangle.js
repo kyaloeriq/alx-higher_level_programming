05dd102f91d5ae6133b0bbb7dfb166a03d564019
kyaloeriq <kyaloeriq@gmail.com>
2024-04-15 02:24:23 -0700

    class Rectangle with a constructor taking 2 args creating an empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,13 +2,13 @@
 
 class Rectangle {
   constructor (w, h) {
-   if (w !== undefined && h !== undefined && Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
-     this.width = w;
-     this.height = h;
-   } else {
-     // If w or h is equal to 0 or not a positive integer, create an empty object
-   }
- }
+    if (w !== undefined && h !== undefined && Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // If w or h is equal to 0 or not a positive integer, create an empty object
+    }
+  }
 }
 
 module.exports = Rectangle;
