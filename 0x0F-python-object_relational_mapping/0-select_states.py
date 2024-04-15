dd27064bbb17511fb9e83d97d24b74070d599e68
kyaloeriq <kyaloeriq@gmail.com>
2024-04-15 03:13:32 -0700

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,12 @@
 import MySQLdb
 import sys
 
+
 def list_states(username, password, database_name):
     """Fetches and prints all states from the specified database"""
     # Connecting to the MySQL server
-    conn = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database_name)
+    conn = MySQLdb.connect(host='localhost', port=3306,
+                           user=username, passwd=password, db=database_name)
     cursor = conn.cursor()
 
     # Query to fetch states
