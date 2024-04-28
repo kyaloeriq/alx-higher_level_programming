8f4517e5c4b50d956630762ef9dce9ec947223b2
kyaloeriq <kyaloeriq@gmail.com>
2024-04-28 05:17:20 -0700

    Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to the provided URL and gets the size of the response body
-response=$(curl -sI "$1" | grep -i '^content-length:' | tr -d '[:space:]' | cut -d ':' -f 2)
+curl -sI "$1" | awk '/Content-Length/ {print $2}'
