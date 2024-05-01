e4770864b0d1bc8277e04cd99d65ceee937616a9
kyaloeriq <kyaloeriq@gmail.com>
2024-05-01 09:42:10 -0700

    script that sends a DELETE request to the URL passed as the first argument and displays the body of the response

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends DELETE request using curl and displays response body
-curl -X DELETE -s -o response_body.txt -w "%{http_code}" "$1" && cat response_body.txt | grep -o "DELETE method accepted"
+# Sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -X DELETE -s -o response_body.txt -w "%{http_code}" "$1" && cat response_body.txt | sed 's/000//g' && echo ""
