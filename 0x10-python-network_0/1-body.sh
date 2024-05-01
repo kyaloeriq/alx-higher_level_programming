9418f755623e4e9f04f0c694edb56c29fc32039c
kyaloeriq <kyaloeriq@gmail.com>
2024-05-01 02:08:08 -0700

    script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -D - "$1" | awk '/^HTTP/{c=$2} END{print c~/^3/?(c~/^30[0-9]$/?"5 redirection with multiple status code":"1 redirection"):"No redirection"}'
+redirect=$(curl -s -D - "$1" | awk '/^HTTP/{c=$2} END{print c~/^3/?(c~/^30[0-9]$/?5:1):0}')
