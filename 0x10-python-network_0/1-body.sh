b926b7b645c0813eb4f49a5ec4674652bacb371e
kyaloeriq <kyaloeriq@gmail.com>
2024-05-01 09:02:52 -0700

    script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-response=$(curl -s -o /dev/null -w "%{http_code}" $1) && [ $response -eq 301 ] && echo "With one redirection"
+response=$(curl -s -o /dev/null -w "%{http_code}" $1) && [ $response -eq 200 ] && curl -s $1
