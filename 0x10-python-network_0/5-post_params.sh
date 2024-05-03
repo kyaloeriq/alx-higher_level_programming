8d5974c0b4ab1cbb573cc5ecb03365cb02c7371b
kyaloeriq <kyaloeriq@gmail.com>
2024-05-03 11:44:36 -0700

    script sends a POST request to the provided URL with email and subject variables

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to the provided URL with email and subject variables
-curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
