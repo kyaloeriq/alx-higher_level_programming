b7ab5e152fbacbbc7a64244f628548ee5ba1974f
kyaloeriq <kyaloeriq@gmail.com>
2024-05-03 12:10:13 -0700

    script to send a GET request to a URL and display the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s $1
+curl -s -w "%{http_code}" -o response.txt $1 | tail -n 1 | grep -q "200" && cat response.txt
