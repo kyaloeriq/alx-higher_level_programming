dbb5fae126f41648cc692db7f68749ea9d59a38c
kyaloeriq <kyaloeriq@gmail.com>
2024-05-03 23:59:58 -0700

    python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Python script that fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    html = response.read().decode('utf-8')
+
+print("- Body response:")
+print("\t- type:", type(html))
+print("\t- content:", html)
