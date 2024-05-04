1c4a3b15fc94d4a502a248b89f5dea9a206362a7
kyaloeriq <kyaloeriq@gmail.com>
2024-05-04 00:13:14 -0700

    python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """ Python script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
+def main():
+    url = 'https://alx-intranet.hbtn.io/status'
 
-url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
+    utf8_content = html.decode('utf-8')
 
-utf8_content = html.decode('utf-8')
+    print("- Body response:")
+    print("\t- type:", type(html))
+    print("\t- content:", html)
+    print("\t- utf8 content:", utf8_content)
 
-print("- Body response:")
-print("\t- type:", type(html))
-print("\t- content:", html)
-print("\t- utf8 content:", utf8_content)
+if __name__ == "__main__":
+    main()
