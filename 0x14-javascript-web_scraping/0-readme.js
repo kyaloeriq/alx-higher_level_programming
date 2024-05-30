0c4f0a7ed114460f28d19d868f4e94aaeea3b886
kyaloeriq <kyaloeriq@gmail.com>
2024-05-30 07:08:47 -0700

    script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const path = process.argv[2];
 
 if (!path) {
-  console.error("Usage: node script.js <file_path>");
+  console.error('Usage: node script.js <file_path>');
   process.exit(1);
 }
 
 fs.readFile(path, 'utf8', (err, data) => {
   if (err) {
-    console.error("An error occurred:", err);
+    console.error('An error occurred:', err);
     return;
   }
   console.log(data);
