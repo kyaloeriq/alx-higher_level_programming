419df4869eb59f57ab2aad5ff2a3b02ed1e71822
kyaloeriq <kyaloeriq@gmail.com>
2024-05-30 09:45:03 -0700

    script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,12 +3,15 @@ const fs = require('fs');
 const path = process.argv[2];
 const content = process.argv[3];
 
-if (!path || !content) {
+if (!path) {
   console.error('Usage: node script.js <file_path> <string_to_write>');
   process.exit(1);
 }
 
-fs.writeFile(path, content, 'utf8', (err) => {
+// If content is undefined, set it to an empty string
+const contentToWrite = content || '';
+
+fs.writeFile(path, contentToWrite, 'utf8', (err) => {
   if (err) {
     console.error(err);
   }
