7c5fdaf8df2e1bb3ed1c6d490321f6ce43867aa4
kyaloeriq <kyaloeriq@gmail.com>
2024-06-01 07:33:27 -0700

    script that prints the title of a Star Wars movie where the episode number matches a given integer

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,39 +1,35 @@
 #!/usr/bin/node
 const request = require('request');
 
-const apiUrl = process.argv[2];
+function getStarWarsMovieTitle (movieId) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-if (!apiUrl) {
-  console.error('Usage: node getStarWarsMovie.js <apiUrl>');
+  request(url, { json: true }, (error, response, body) => {
+    if (error) {
+      console.error('Error fetching movie data:', error);
+      return;
+    }
+
+    if (response.statusCode === 200) {
+      console.log(`The title of the Star Wars movie with episode number ${movieId} is '${body.title}'.`);
+    } else {
+      console.log(`No movie found with episode number ${movieId}.`);
+    }
+  });
+}
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Usage: node script.js <movie_id>');
   process.exit(1);
 }
 
-const wedgeId = `https://swapi-api.alx-tools.com/api/people/18/';
+const movieIdInt = parseInt(movieId);
 
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('An error occurred:', error);
-    return;
-  }
+if (isNaN(movieIdInt)) {
+  console.log('The movie ID must be an integer.');
+  process.exit(1);
+}
 
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data, status code: ${response.statusCode}`);
-    return;
-  }
-
-  try {
-    const data = JSON.parse(body);
-    const films = data.results;
-    let wedgeCount = 0;
-
-    films.forEach((film) => {
-      if (film.characters.includes(wedgeId)) {
-        wedgeCount++;
-      }
-    });
-
-    console.log(wedgeCount);
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
-  }
-});
+getStarWarsMovieTitle(movieIdInt);
